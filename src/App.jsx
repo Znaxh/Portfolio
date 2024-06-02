13edde78fc3c2f91ed52489217fd90a210a6cabd
AnuragSingh4845 <anuragpratapsingh4845@gmail.com>
2024-06-02 19:51:52 +0530

    further progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { Outlet } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from './App'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element : <App />,
+    children : [
+      {
+        path : "",
+        element: <Home />
+      },{
+        path : 'about',
+        element: <About />
+      },{
+        path : 'contact',
+        element : <Contact />
+      },{
+        path : 'projects',
+        element : <Projects />
+      }
+    ] 
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
