55d28dac11309aff967802404e768413cf0ac0de
AnuragSingh4845 <anuragpratapsingh4845@gmail.com>
2024-06-03 22:59:10 +0530

    Progress

---
+++ src/Pages/Resume.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Resume = () => {
+  return (
+    <>
+      <div className='pixel-button'>Download CV</div>
+    </>
+  )
+}
+
+export default Resume
---
+++ src/components/Home/Intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Intro = () => {
+  return (
+    <div>
+        <div>Let Me Introduce Myself</div>
+        <div>Whenever possible, I also apply my passion for developing products with Node.js and Modern Javascript Library and Frameworks  like React.js and Next.js</div>
+        <div>My field of Interest's are building new  Web Technologies and Products and also in areas related to Blockchain.</div>
+        <div>I am fluent in classics like C++, Javascript and Go.</div>
+    </div>
+  )
+}
+
+export default Intro
