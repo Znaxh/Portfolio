e6c0032acd138cedf6d31eba006fef4ef11bbb13
AnuragSingh4845 <anuragpratapsingh4845@gmail.com>
2024-06-03 23:03:29 +0530

    Phew New Github calender typewritter footer

---
+++ src/components/About/Techstack.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+} from "react-icons/di";
+
+const Techstack = () => {
+  return (
+    <div>
+        <DiJavascript1 />
+        <CgCPlusPlus />
+        <DiReact />
+        <DiNodejs />
+        <DiMongodb />
+        <DiPython />
+        <DiGit />
+        
+    </div>
+  )
+}
+
+export default Techstack
