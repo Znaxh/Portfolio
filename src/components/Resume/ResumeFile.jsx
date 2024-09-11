ac395e2beaaf2788132b72d69ee42c89727f8b62
Anurag <137142327+Znaxh@users.noreply.github.com>
2024-09-11 10:07:15 +0000

    resume image size changed

--- src/components/Resume/ResumeFile.jsx
+++ src/components/Resume/ResumeFile.jsx
@@ -5,7 +5,7 @@ const ResumeFile = () => {
   return (
     <div className='justify-center flex mt-16 px-2 '>
       <div className="absolute inset-x-0 top-0 h-1 bg-gradient-to-r from-red-500 via-purple-500 to-blue-500"></div>
-        <img src={resumeimage} alt="resume.jpg" />
+        <img src={resumeimage} className="h-[1200px]" alt="resume.jpg" />
     </div>
   )
 }
